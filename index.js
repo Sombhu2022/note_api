8c9dc2718bf5df8fbd0decc2b8736da484a090cd
Sombhu das <105740734+Sombhu2022@users.noreply.github.com>
2023-12-20 16:53:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ import cors from 'cors'
 database();
 server.use(cors({
 
-   origin: process.env.FRONTEND_URL || "http://localhost:3000" ,
+   origin: [ process.env.FRONTEND_URL , "http://localhost:3000" ] ,
 
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     credentials: true,
