2a3fbd939491d41a651227eaf989f28bcd81b481
Sombhu das <105740734+Sombhu2022@users.noreply.github.com>
2023-12-21 22:45:57 +0530

    Update userAuthentication.js

--- middlewares/userAuthentication.js
+++ middlewares/userAuthentication.js
@@ -3,7 +3,7 @@ import { Users } from '../models/userModel.js';
 
 export const isAuthenticate=async (req , res , next)=>{
     
-    const token = req.Cookies.token;
+    const token = req.cookies.token;
 
     if(!token) return res.status(400).json({
         message:'login first'
