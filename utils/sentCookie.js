00eaa7463834687a0e0fecd3e61155200cafe9e5
Sombhu das <105740734+Sombhu2022@users.noreply.github.com>
2023-12-22 17:40:02 +0530

    Update sentCookie.js

--- utils/sentCookie.js
+++ utils/sentCookie.js
@@ -11,7 +11,7 @@ export const sendCookic = (user, res, message, statusCode = 200) => {
     .cookie("token",token , {
       expires: new Date(Date.now() + 3600000), // Set expiration time
       httpOnly: true,
-
+      sameSite: "None",
       secure:true,
 
     })
