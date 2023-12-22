d9219e6a489107992a6c8c3a3358fa5d350dcdf6
Sombhu2022 <sombhudas93@gmail.com>
2023-12-22 17:07:35 +0530

    bug fix

--- utils/sentCookie.js
+++ utils/sentCookie.js
@@ -1,6 +1,6 @@
 import jwt from "jsonwebtoken";
 
-export const sendCookic = (user, res, message, statusCode = 200) => {
+  export const sendCookic = (user, res, message, statusCode = 200) => {
   const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, {
     expiresIn: "1h",
   });
@@ -9,6 +9,7 @@ export const sendCookic = (user, res, message, statusCode = 200) => {
     .cookie("token",token , {
       expires: new Date(Date.now() + 3600000), // Set expiration time
       httpOnly: true,
+      secure:true,
     })
     .json({
       success: true,
