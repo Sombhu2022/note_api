e23ef85e7ffcf3df74d5ff679289bd7b4071cc9a
Sombhu2022 <sombhudas93@gmail.com>
2024-03-20 17:58:22 +0530

    final

--- controller/noteControler.js
+++ controller/noteControler.js
@@ -4,13 +4,13 @@ import { v2 as cloudinary } from 'cloudinary';
 export const getNotes = async (req, res) => {
   try {
     const {id}=req.user
-    console.log(id);
+    // console.log(id);
 
     const note = await Notes.find({user:id}).populate("user");
 
     if(!note) return res.status(200).json("no data are here.")
 
-    console.log(note);
+    // console.log(note);
     res.status(200).json({
       success: true,
       note,
@@ -46,19 +46,22 @@ export const getNote = async (req, res) => {
 export const updateNote = async (req, res) => {
   try {
 
-    const {note , subject} = req.body;
-    const {id}=req.user
-
-    const data = await Notes.findByIdAndUpdate({ _id: id }, {
-         note,
+    const {title , subject} = req.body;
+    const {id}=req.params
+    // console.log(title , subject);
+    await Notes.findByIdAndUpdate({ _id: id }, {
+         title,
          subject,
     } , {
       new: true,
     });
+    
+    console.log(req.user);
+    const note = await Notes.find({_id: req.user._id})
 
     res.status(200).json({
       success: true,
-      data,
+      note,
     });
   } catch (error) {
     res.status(400).json({
@@ -75,38 +78,36 @@ export const addNote = async (req, res) => {
     const { id }=req.user
     // console.log(id , req.body)
 
-    let data ; 
+    let TempImage ; 
 
     if(image !== 'null'){
-      // console.log("if");
+ 
       const profilePic = await cloudinary.uploader.upload(image , {
         folder:"notebook"
     })
     // console.log(profilePic);
-    const TempImage = {
+     TempImage = {
         url:profilePic.secure_url,
         image_id:profilePic.public_id,
     }
+  }
+  else{
+
+    TempImage = {
+      url:'',
+      image_id:'',
+  }
+  
+  }
     // console.log(title ,subject)
-    data = await Notes.create({
+  const  data = await Notes.create({
       title,
       subject,
       user:id,
       image:TempImage
     });
-  }
   
-  else{
-    // console.log("else");
-    data = await Notes.create({
-      title,
-      subject,
-      user:id,
-    });
-   console.log(data);
-   
-  }
-
+  
   const note = await data.populate('user')
   res.status(200).json({
       note , 
@@ -127,11 +128,21 @@ export const addNote = async (req, res) => {
 export const deletNote = async (req, res) => {
   try {
     const { id } = req.params;
+    
 
-    await Notes.findByIdAndDelete({ _id: id });
+    const data = await Notes.findByIdAndDelete({ _id: id });
+    console.log("delete",data);
+    if(data.image.image_id){
+      await cloudinary.uploader.destroy(data.image.image_id);
+      console.log("image deleted");
+    }
+    console.log(req.user);
+    const note = await Notes.find({user: req.user._id})
+
     res.status(200).json({
       success: true,
       message: "delete note",
+      note
     });
   } catch (error) {
     res.status(400).json({
