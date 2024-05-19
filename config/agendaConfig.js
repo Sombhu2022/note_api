8fb393852da7594d88a0453dd27e098de86e3a0a
Sombhu2022 <sombhudas93@gmail.com>
2024-05-20 00:13:43 +0530

    fix 2

---
+++ config/agendaConfig.js
@@ -0,0 +1,34 @@
+import Agenda from "agenda";
+import { sendEmail } from "../utils/sendMail.js";
+
+const mongoConnectionString = process.env.AGENDA_DB_URL;
+// const mongoConnectionString = "mongodb://127.0.0.1:27017/agenda";
+console.log(mongoConnectionString);
+export const agenda = new Agenda({
+    db: { address: mongoConnectionString , collection: 'Jobs' },
+    //   processEvery: '30 seconds',
+      options: { useNewUrlParser: true }
+});
+
+export const agendaConnection =async()=>{
+    
+agenda.on('ready', async() => {
+    console.log('Agenda started!');
+    await agenda.start();
+});
+
+agenda.on('error', (error) => {
+    console.error('Agenda connection error:', error);
+});
+
+
+agenda.define('send email', async (job) => {
+    const { to, subject, body , sendAt } = job.attrs.data;
+    sendEmail(to , subject , `${body}  ${sendAt} `)
+    console.log(sendAt);
+  });
+
+ console.log("url",process.env.DB_URL);
+}
+
+
--- controller/noteControler.js
+++ controller/noteControler.js
@@ -2,6 +2,7 @@ import { Notes } from "../models/noteModel.js";
 import { v2 as cloudinary } from 'cloudinary';
 import { CronJob } from 'cron';
 import { sendEmail } from "../utils/sendMail.js";
+import { agenda } from "../config/agendaConfig.js";
 // import { agenda } from "../index.js";
 
 
@@ -137,11 +138,11 @@ export const addNote = async (req, res) => {
     });
 
 
-    // const scheduleEmail =async({to , subject ,body , sendAt})=>{
-    //   await agenda.schedule(sendAt ,'send email', {to , subject , body , sendAt})
-    // }
+    const scheduleEmail =async({to , subject ,body , sendAt})=>{
+      await agenda.schedule(sendAt ,'send email', {to , subject , body , sendAt})
+    }
  
-    // scheduleEmail({ to:req?.user?.email , subject:`We are reminding you :- ${title}` , body: `Hii ${req?.user?.name}, Do you remember this ...  \n ${subject} `, sendAt:noticeTime} )
+    scheduleEmail({ to:req?.user?.email , subject:`We are reminding you :- ${title}` , body: `Hii ${req?.user?.name}, Do you remember this ...  \n ${subject} `, sendAt:noticeTime} )
   
   
   const note = await data.populate('user')
